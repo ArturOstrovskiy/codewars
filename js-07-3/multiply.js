3237a6f69f7a77065d1c82f4ebd132eb65f4bc5d
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-10-14 15:08:08 +0300

    commit

---
+++ js-07-3/multiply.js
@@ -0,0 +1,16 @@
+// DESCRIPTION
+// This code does not execute properly. Try to figure out why.*/
+
+// TEST
+/*
+describe("Multiply", () => {
+	it("fixed tests", () => {
+		assert.strictEqual(multiply(1,1), 1);
+		assert.strictEqual(multiply(2,1), 2);
+		*/
+
+// SOLUTION
+function multiply(a, b){
+	return a * b
+}
+console.log(multiply(2,2))
